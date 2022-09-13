c765629c40d68c8a3b202dabcd0a1da8dd0b2399
Nelsoneric <egwunelsononc@gmail.com>
2022-09-12 20:50:15 -0700

    v

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@ Task 5
 
 #include<stdio.h>
 /**
- *  *main - print string
- *   *
- *    *Return: Always 0
- *     */
+*  *main - print string
+*   *
+*    *Return: Always 0
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
