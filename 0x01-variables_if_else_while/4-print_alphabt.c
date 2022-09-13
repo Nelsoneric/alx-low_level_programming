d8bdbb0723cac32c376e438564faaa1504b3c6f0
Nelsoneric <egwunelsononc@gmail.com>
2022-09-12 20:07:36 -0700

    o

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (leter != 'e' && letter != 'q')
+		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
 	putchar('\n');
